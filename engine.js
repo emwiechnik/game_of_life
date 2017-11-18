7889b4f3662f4325708425e67b988aff210ab595
Marcin <emwiechnik@gmail.com>
2017-11-18 01:22:21 +0100

    Corrected the logic and implemented the original game of life rules

--- engine.js
+++ engine.js
@@ -1,7 +1,26 @@
+var root = {
+    gameScreenGrid: undefined,
+    game: undefined
+}
+
 function Creature() {
     this.isAlive = true;
-    this.die = function() {
-        this.isAlive = false;
+    this.dieNextTime = false;
+    this.comeToLifeNextTime = false;
+
+    this.die = function () {
+        this.dieNextTime = true;
+    }
+    this.comeToLife = function () {
+        this.comeToLifeNextTime = true;
+    }
+    this.doWhatYouNeedToDo = function() {
+        if (this.dieNextTime) {
+            this.isAlive = false;
+        } else if (this.comeToLifeNextTime) {
+            this.isAlive = true;
+        } 
+        this.dieNextTime = this.comeToLifeNextTime = false;
     }
 }
 
@@ -9,55 +28,132 @@ function GameScreenGrid(settings) {
     screen = document.getElementById('game-screen');
 
     var private = {
-        canvas : screen.getContext('2d'),
-        settings : settings
+        canvas: screen.getContext('2d'),
+        settings: settings
     }
-    
-    this.draw = function(row, col, creature) {
+
+    this.draw = function (row, col, creature) {
         aliveColor = private.settings.aliveColor;
         deadColor = private.settings.deadColor;
         cellSize = private.settings.cellSize;
         color = creature.isAlive ? aliveColor : deadColor;
         private.canvas.fillStyle = color;
-        private.canvas.fillRect(col * cellSize, row * cellSize, col * cellSize + cellSize, row * cellSize + cellSize);
+        x1 = col * cellSize;
+        y1 = row * cellSize;
+        x2 = x1 + cellSize;
+        y2 = y1 + cellSize;
+        private.canvas.fillRect(x1, y1, cellSize, cellSize);
     }
 }
 
+window.requestAnimFrame = (function (callback) {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 30);
+        };
+})();
+
 function GameOrchestrator(gameScreenGrid, settings) {
-    var initializeCreatures = function(count) {
+    var initializeCreatures = function (count) {
         n = Math.floor(Math.sqrt(count));
 
         arr = new Array(n);
-        
-        for(i = 0; i < n; ++i) {
+
+        for (i = 0; i < n; ++i) {
             arr[i] = new Array(n);
         }
-        
-        for(x = 0; x < n; ++x) {
-            for(y = 0; y < n; ++y) {
+
+        for (x = 0; x < n; ++x) {
+            for (y = 0; y < n; ++y) {
                 arr[x][y] = new Creature();
-                // for now just random setup
-                if (Math.random() > 0.5) {
-                    arr[x][y].isAlive = false;
+                
+                if (Math.random() > 0.1) {
+                     arr[x][y].isAlive = false;
                 }
             }
         }
-        
+
         return arr;
     }
 
     var private = {
-        creatures2dArray : initializeCreatures(settings.creaturesCount),
-        gameScreenGrid : gameScreenGrid
+        creatures2dArray: initializeCreatures(settings.creaturesCount),
+        gameScreenGrid: gameScreenGrid
     }
 
-    this.display = function() {
+    display = function () {
         n = private.creatures2dArray.length;
-        for(x = 0; x < n; ++x) {
-            for(y = 0; y < n; ++y) {
+        for (x = 0; x < n; ++x) {
+            for (y = 0; y < n; ++y) {
                 private.gameScreenGrid.draw(y, x, private.creatures2dArray[x][y]);
             }
         }
+    }
+
+    goToNextIteration = function () {
+        n = private.creatures2dArray.length;
+        for (x = 0; x < n; ++x) {
+            for (y = 0; y < n; ++y) {
+                private.creatures2dArray[x][y].doWhatYouNeedToDo();
+            }
+        }
+    }
+
+    countNeighbours = function (x, y) {
+        count = 0;
+        n = private.creatures2dArray.length;
+        xMin = Math.max(0, x - 1);
+        xMax = Math.min(n-1, x + 1);
+        yMin = Math.max(0, y - 1);
+        yMax = Math.min(n-1, y + 1);
+
+        for (i = xMin; i <= xMax; ++i) {
+            for (j = yMin; j <= yMax; ++j) {
+                if (i == x && j == y) continue;
+                count += private.creatures2dArray[i][j].isAlive ? 1 : 0;
+            }
+        }
+
+        return count;
+    }
+
+    applyRules = function () {
+        for (x = 0; x < n; ++x) {
+            for (y = 0; y < n; ++y) {
+                nCount = countNeighbours(x, y);
+                
+                // applying rules
+                if (private.creatures2dArray[x][y].isAlive) {
+                    if (nCount < 2 || nCount > 3) {
+                        private.creatures2dArray[x][y].die();
+                    }
+                }
+
+                if (private.creatures2dArray[x][y].isAlive === false) {
+                    if (nCount === 3) {
+                        private.creatures2dArray[x][y].comeToLife();
+                    }
+                }
+            }
+        }
+    }
+
+    evolve = function () {
+        if (private.running) {
+            applyRules();
+            display();
+            goToNextIteration();
+            requestAnimFrame(evolve);
+        }
+    }
+
+    this.run = function () {
+        private.running = true;
+        evolve();
+    }
+
+    this.stop = function () {
+        private.running = false;
     }
 }
 
@@ -68,8 +164,13 @@ function getSettingValue(id) {
 
 function start() {
     {
-        var gameScreenGrid = new GameScreenGrid({ cellSize : 20, aliveColor : 'blue', deadColor : 'white' });
-        var game = new GameOrchestrator(gameScreenGrid, { creaturesCount : getSettingValue('creatures-count') });
-        game.display();
+        root.gameScreenGrid = new GameScreenGrid({ cellSize: 5, aliveColor: 'blue', deadColor: 'white' });
+        root.game = new GameOrchestrator(root.gameScreenGrid, { creaturesCount: getSettingValue('creatures-count') });
+        root.game.run();
     }
+}
+
+function apply() {
+    root.game.stop();
+    start();
 }
